54bbaab2c9541660fc65d159fcbe60698b63197d
Urffin-Juss <mellondefall@gmail.com>
2026-01-02 16:21:55 +0300

    hmmm

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+from dataclasses import dataclass
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@dataclass(frozen=True)
+class DBConfig:
+    host: str = os.getenv("DB_HOST")
+    port: str = os.getenv("DB_PORT")
+    username: str = os.getenv("DB_USERNAME")
+    password: str = os.getenv("DB_PASSWORD")
+
+def get_db_config() -> DBConfig:
+    return DBConfig(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME", "hh_project"),
+        user=os.getenv("DB_USER", "postgres_admin"),
+        password=os.getenv("DB_PASSWORD", "!QAZxsw2")
+    )
